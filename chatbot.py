219c3da123635854c180c67ea422cc48b8d4bd5f
ozgecitak <ozgecitakk@gmail.com>
2025-10-22 23:01:31 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import RetrievalQA
 from langchain.chains import create_stuff_documents_chain
 
 # -------------------------------
@@ -147,6 +147,7 @@ def main():
 # -------------------------------
 if __name__ == "__main__":
     main()
+
 
 
 
